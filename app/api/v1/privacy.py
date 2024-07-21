ef981421b781aa8881362d97c53d08a308364d2c
ehsansh84 <ehsan.shirzadi@gmail.com>
2024-07-21 23:35:42 +0200

    updated: path

--- app/api/v1/privacy.py
+++ app/api/v1/privacy.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from publics import Settings
 
 module_name = 'pages'
-# module_text = ''
 router = APIRouter(
     prefix=f"/{module_name}",
     tags=[module_name]
 )
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=Settings.TEMPLATE_DIR)
 
 
 @router.get("/privacy_policy", response_class=HTMLResponse)
